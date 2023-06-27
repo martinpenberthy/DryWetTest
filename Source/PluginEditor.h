1d10e91db61a3c622fb9a8547bd06824b8d20866
Martin <martinpenberthy1@gmail.com>
2023-06-27 14:23:38 -0700

    first commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class DryWetTestAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    DryWetTestAudioProcessorEditor (DryWetTestAudioProcessor&);
+    ~DryWetTestAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    
+    juce::Slider sliderPreGain;
+    juce::Label labelPreGain;
+    
+    juce::Slider sliderMix;
+    juce::Label labelMix;
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    DryWetTestAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DryWetTestAudioProcessorEditor)
+};
